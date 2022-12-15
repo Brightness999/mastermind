472d0939af36fd45a50403ae0dfcb2bd7cb2ac88
wp-dream111 <dmitriymazilin@yahoo.com>
2022-12-16 01:09:27 +0600

    chore: fix notification updating issue

--- src/routes/Notifications/components/index.js
+++ src/routes/Notifications/components/index.js
@@ -13,7 +13,6 @@ class NotificationSetting extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      notificationSetting: undefined,
       isNewSessionEmail: false,
       isNewSessionPush: false,
       isNewSessionText: false,
@@ -39,11 +38,56 @@ class NotificationSetting extends React.Component {
   }
 
   componentDidMount() {
-    this.setState({ notificationSetting: this.props.user.notificationSetting });
+    this.setState({
+      isNewSessionEmail: this.props.user.notificationSetting?.isNewSessionEmail,
+      isNewSessionPush: this.props.user.notificationSetting?.isNewSessionPush,
+      isNewSessionText: this.props.user.notificationSetting?.isNewSessionText,
+      isRescheduleSessionEmail: this.props.user.notificationSetting?.isRescheduleSessionEmail,
+      isRescheduleSessionPush: this.props.user.notificationSetting?.isRescheduleSessionPush,
+      isRescheduleSessionText: this.props.user.notificationSetting?.isRescheduleSessionText,
+      isSessionReminderEmail: this.props.user.notificationSetting?.isSessionReminderEmail,
+      isSessionReminderPush: this.props.user.notificationSetting?.isSessionReminderPush,
+      isSessionReminderText: this.props.user.notificationSetting?.isSessionReminderText,
+      isCancelSessionEmail: this.props.user.notificationSetting?.isCancelSessionEmail,
+      isCancelSessionPush: this.props.user.notificationSetting?.isCancelSessionPush,
+      isCancelSessionText: this.props.user.notificationSetting?.isCancelSessionText,
+      isSubsidyUpdateEmail: this.props.user.notificationSetting?.isSubsidyUpdateEmail,
+      isSubsidyUpdatePush: this.props.user.notificationSetting?.isSubsidyUpdatePush,
+      isSubsidyUpdateText: this.props.user.notificationSetting?.isSubsidyUpdateText,
+      isFlagClearedEmail: this.props.user.notificationSetting?.isFlagClearedEmail,
+      isFlagClearedPush: this.props.user.notificationSetting?.isFlagClearedPush,
+      isFlagClearedText: this.props.user.notificationSetting?.isFlagClearedText,
+      isFlagCreatedEmail: this.props.user.notificationSetting?.isFlagCreatedEmail,
+      isFlagCreatedPush: this.props.user.notificationSetting?.isFlagCreatedPush,
+      isFlagCreatedText: this.props.user.notificationSetting?.isFlagCreatedText,
+    });
   }
 
   handleSaveSetting = () => {
-    request.post(updateNotificationSetting).then(res => {
+    const data = {
+      isNewSessionEmail: this.state.isNewSessionEmail,
+      isNewSessionPush: this.state.isNewSessionPush,
+      isNewSessionText: this.state.isNewSessionText,
+      isRescheduleSessionEmail: this.state.isRescheduleSessionEmail,
+      isRescheduleSessionPush: this.state.isRescheduleSessionPush,
+      isRescheduleSessionText: this.state.isRescheduleSessionText,
+      isSessionReminderEmail: this.state.isSessionReminderEmail,
+      isSessionReminderPush: this.state.isSessionReminderPush,
+      isSessionReminderText: this.state.isSessionReminderText,
+      isCancelSessionEmail: this.state.isCancelSessionEmail,
+      isCancelSessionPush: this.state.isCancelSessionPush,
+      isCancelSessionText: this.state.isCancelSessionText,
+      isSubsidyUpdateEmail: this.state.isSubsidyUpdateEmail,
+      isSubsidyUpdatePush: this.state.isSubsidyUpdatePush,
+      isSubsidyUpdateText: this.state.isSubsidyUpdateText,
+      isFlagClearedEmail: this.state.isFlagClearedEmail,
+      isFlagClearedPush: this.state.isFlagClearedPush,
+      isFlagClearedText: this.state.isFlagClearedText,
+      isFlagCreatedEmail: this.state.isFlagCreatedEmail,
+      isFlagCreatedPush: this.state.isFlagCreatedPush,
+      isFlagCreatedText: this.state.isFlagCreatedText,
+    }
+    request.post(updateNotificationSetting, data).then(res => {
       const { success } = res;
       if (success) {
         message.success("Successfully updated");
@@ -55,7 +99,29 @@ class NotificationSetting extends React.Component {
   }
 
   render() {
-    const { notificationSetting } = this.state;
+    const {
+      isNewSessionEmail,
+      isNewSessionPush,
+      isNewSessionText,
+      isRescheduleSessionEmail,
+      isRescheduleSessionPush,
+      isRescheduleSessionText,
+      isSessionReminderEmail,
+      isSessionReminderPush,
+      isSessionReminderText,
+      isCancelSessionEmail,
+      isCancelSessionPush,
+      isCancelSessionText,
+      isSubsidyUpdateEmail,
+      isSubsidyUpdatePush,
+      isSubsidyUpdateText,
+      isFlagClearedEmail,
+      isFlagClearedPush,
+      isFlagClearedText,
+      isFlagCreatedEmail,
+      isFlagCreatedPush,
+      isFlagCreatedText,
+    } = this.state;
 
     return (
       <div className="full-layout page usermanager-page">
@@ -87,27 +153,27 @@ class NotificationSetting extends React.Component {
                       </tr>
                       <tr>
                         <td>New Session</td>
-                        <td><div className='text-center'><Switch defaultChecked={notificationSetting?.isNewSessionEmail} onChange={v => this.setState({ isNewSessionEmail: v })} /></div></td>
-                        <td><div className='text-center'><Switch defaultChecked={notificationSetting?.isNewSessionText} onChange={v => this.setState({ isNewSessionText: v })} /></div></td>
-                        <td><div className='text-center'><Switch defaultChecked={notificationSetting?.isNewSessionPush} onChange={v => this.setState({ isNewSessionPush: v })} /></div></td>
+                        <td><div className='text-center'><Switch defaultChecked={isNewSessionEmail} onChange={v => this.setState({ isNewSessionEmail: v })} /></div></td>
+                        <td><div className='text-center'><Switch defaultChecked={isNewSessionText} onChange={v => this.setState({ isNewSessionText: v })} /></div></td>
+                        <td><div className='text-center'><Switch defaultChecked={isNewSessionPush} onChange={v => this.setState({ isNewSessionPush: v })} /></div></td>
                       </tr>
                       <tr>
                         <td>Reschedule Session</td>
-                        <td><div className='text-center'><Switch defaultChecked={notificationSetting?.isRescheduleSessionEmail} onChange={v => this.setState({ isRescheduleSessionEmail: v })} /></div></td>
-                        <td><div className='text-center'><Switch defaultChecked={notificationSetting?.isRescheduleSessionText} onChange={v => this.setState({ isRescheduleSessionText: v })} /></div></td>
-                        <td><div className='text-center'><Switch defaultChecked={notificationSetting?.isRescheduleSessionPush} onChange={v => this.setState({ isRescheduleSessionPush: v })} /></div></td>
+                        <td><div className='text-center'><Switch defaultChecked={isRescheduleSessionEmail} onChange={v => this.setState({ isRescheduleSessionEmail: v })} /></div></td>
+                        <td><div className='text-center'><Switch defaultChecked={isRescheduleSessionText} onChange={v => this.setState({ isRescheduleSessionText: v })} /></div></td>
+                        <td><div className='text-center'><Switch defaultChecked={isRescheduleSessionPush} onChange={v => this.setState({ isRescheduleSessionPush: v })} /></div></td>
                       </tr>
                       <tr>
                         <td>Cancel Session</td>
-                        <td><div className='text-center'><Switch defaultChecked={notificationSetting?.isCancelSessionEmail} onChange={v => this.setState({ isCancelSessionEmail: v })} /></div></td>
-                        <td><div className='text-center'><Switch defaultChecked={notificationSetting?.isCancelSessionText} onChange={v => this.setState({ isCancelSessionText: v })} /></div></td>
-                        <td><div className='text-center'><Switch defaultChecked={notificationSetting?.isCancelSessionPush} onChange={v => this.setState({ isCancelSessionPush: v })} /></div></td>
+                        <td><div className='text-center'><Switch defaultChecked={isCancelSessionEmail} onChange={v => this.setState({ isCancelSessionEmail: v })} /></div></td>
+                        <td><div className='text-center'><Switch defaultChecked={isCancelSessionText} onChange={v => this.setState({ isCancelSessionText: v })} /></div></td>
+                        <td><div className='text-center'><Switch defaultChecked={isCancelSessionPush} onChange={v => this.setState({ isCancelSessionPush: v })} /></div></td>
                       </tr>
                       <tr>
                         <td>Session reminder</td>
-                        <td><div className='text-center'><Switch defaultChecked={notificationSetting?.isSessionReminderEmail} onChange={v => this.setState({ isSessionReminderEmail: v })} /></div></td>
-                        <td><div className='text-center'><Switch defaultChecked={notificationSetting?.isSessionReminderText} onChange={v => this.setState({ isSessionReminderText: v })} /></div></td>
-                        <td><div className='text-center'><Switch defaultChecked={notificationSetting?.isSessionReminderPush} onChange={v => this.setState({ isSessionReminderPush: v })} /></div></td>
+                        <td><div className='text-center'><Switch defaultChecked={isSessionReminderEmail} onChange={v => this.setState({ isSessionReminderEmail: v })} /></div></td>
+                        <td><div className='text-center'><Switch defaultChecked={isSessionReminderText} onChange={v => this.setState({ isSessionReminderText: v })} /></div></td>
+                        <td><div className='text-center'><Switch defaultChecked={isSessionReminderPush} onChange={v => this.setState({ isSessionReminderPush: v })} /></div></td>
                       </tr>
                       <tr>
                         <td colSpan={4} className="bg-pastel">
@@ -116,9 +182,9 @@ class NotificationSetting extends React.Component {
                       </tr>
                       <tr>
                         <td>Subsidy Update</td>
-                        <td><div className='text-center'><Switch defaultChecked={notificationSetting?.isSubsidyUpdateEmail} onChange={v => this.setState({ isSubsidyUpdateEmail: v })} /></div></td>
-                        <td><div className='text-center'><Switch defaultChecked={notificationSetting?.isSubsidyUpdateText} onChange={v => this.setState({ isSubsidyUpdateText: v })} /></div></td>
-                        <td><div className='text-center'><Switch defaultChecked={notificationSetting?.isSubsidyUpdatePush} onChange={v => this.setState({ isSubsidyUpdatePush: v })} /></div></td>
+                        <td><div className='text-center'><Switch defaultChecked={isSubsidyUpdateEmail} onChange={v => this.setState({ isSubsidyUpdateEmail: v })} /></div></td>
+                        <td><div className='text-center'><Switch defaultChecked={isSubsidyUpdateText} onChange={v => this.setState({ isSubsidyUpdateText: v })} /></div></td>
+                        <td><div className='text-center'><Switch defaultChecked={isSubsidyUpdatePush} onChange={v => this.setState({ isSubsidyUpdatePush: v })} /></div></td>
                       </tr>
                       <tr>
                         <td colSpan={4} className="bg-pastel">
@@ -127,15 +193,15 @@ class NotificationSetting extends React.Component {
                       </tr>
                       <tr>
                         <td>Flag Created</td>
-                        <td><div className='text-center'><Switch defaultChecked={notificationSetting?.isFlagCreatedEmail} onChange={v => this.setState({ isFlagCreatedEmail: v })} /></div></td>
-                        <td><div className='text-center'><Switch defaultChecked={notificationSetting?.isFlagCreatedText} onChange={v => this.setState({ isFlagCreatedText: v })} /></div></td>
-                        <td><div className='text-center'><Switch defaultChecked={notificationSetting?.isFlagCreatedPush} onChange={v => this.setState({ isFlagCreatedPush: v })} /></div></td>
+                        <td><div className='text-center'><Switch defaultChecked={isFlagCreatedEmail} onChange={v => this.setState({ isFlagCreatedEmail: v })} /></div></td>
+                        <td><div className='text-center'><Switch defaultChecked={isFlagCreatedText} onChange={v => this.setState({ isFlagCreatedText: v })} /></div></td>
+                        <td><div className='text-center'><Switch defaultChecked={isFlagCreatedPush} onChange={v => this.setState({ isFlagCreatedPush: v })} /></div></td>
                       </tr>
                       <tr>
                         <td>Flag Cleared</td>
-                        <td><div className='text-center'><Switch defaultChecked={notificationSetting?.isFlagClearedEmail} onChange={v => this.setState({ isFlagClearedEmail: v })} /></div></td>
-                        <td><div className='text-center'><Switch defaultChecked={notificationSetting?.isFlagClearedText} onChange={v => this.setState({ isFlagClearedText: v })} /></div></td>
-                        <td><div className='text-center'><Switch defaultChecked={notificationSetting?.isFlagClearedPush} onChange={v => this.setState({ isFlagClearedPush: v })} /></div></td>
+                        <td><div className='text-center'><Switch defaultChecked={isFlagClearedEmail} onChange={v => this.setState({ isFlagClearedEmail: v })} /></div></td>
+                        <td><div className='text-center'><Switch defaultChecked={isFlagClearedText} onChange={v => this.setState({ isFlagClearedText: v })} /></div></td>
+                        <td><div className='text-center'><Switch defaultChecked={isFlagClearedPush} onChange={v => this.setState({ isFlagClearedPush: v })} /></div></td>
                       </tr>
                     </tbody>
                   </table>
