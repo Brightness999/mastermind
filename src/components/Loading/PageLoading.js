46ca82bda3687204f8fd0c7467519f2225d30160
wp-dream111 <dmitriymazilin@yahoo.com>
2023-02-14 22:04:38 +0600

    chore: update PageLoading

--- src/components/Loading/PageLoading.js
+++ src/components/Loading/PageLoading.js
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
  * Loading effect example
  */
 const PageLoading = ({ loading, style = 'style1', isBackground = false }) =>
-  loading ? <div className={`loading-spinner loading-spinner-${style}`} /> : null;
+  loading ? <div className={`${isBackground ? 'loading-spinner-background' : ''}`}><div className={`loading-spinner loading-spinner-${style}`} /></div> : null;
 
 PageLoading.propTypes = {
   loading: PropTypes.bool,
