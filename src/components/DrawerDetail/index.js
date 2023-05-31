b446800bca42cdc9d5a162432b0e9171436a2371
wp-dream111 <dmitriymazilin@yahoo.com>
2023-05-31 10:37:52 -0400

    fix rate, requestclearance, payinvoice show in DrawerDetail

--- src/components/DrawerDetail/index.js
+++ src/components/DrawerDetail/index.js
@@ -978,7 +978,7 @@ class DrawerDetail extends Component {
           {[EVALUATION, APPOINTMENT, SUBSIDY].includes(event?.type) && (
             <div className='detail-item flex'>
               <p className='font-18 font-700 title'>{intl.formatMessage(msgCreateAccount.rate)}</p>
-              <p className={`font-18 ${event?.status === CLOSED ? 'text-underline cursor' : ''} ${!event?.isPaid && 'text-red'}`} onClick={() => event?.status === CLOSED && this.setState({ visibleInvoice: true })}>${event?.rate}</p>
+              <p className={`font-18 ${event?.status === CLOSED ? 'text-underline cursor' : ''} ${!event?.sessionInvoice?.isPaid && 'text-red'}`} onClick={() => event?.status === CLOSED && this.setState({ visibleInvoice: true })}>${event?.rate}</p>
             </div>
           )}
           {[SCREEN, CONSULTATION].includes(event?.type) && (
@@ -1174,7 +1174,7 @@ class DrawerDetail extends Component {
                   </Button>
                 </Col>
               )}
-              {[EVALUATION, APPOINTMENT, SUBSIDY].includes(event?.type) && event?.status === CLOSED && !event?.isPaid && (userRole === 3 || userRole > 900) && (
+              {[EVALUATION, APPOINTMENT, SUBSIDY].includes(event?.type) && event?.status === CLOSED && !event?.sessionInvoice?.isPaid && (userRole === 3 || userRole > 900) && (
                 <Col span={12}>
                   <form aria-live="polite" data-ux="Form" action="https://www.paypal.com/cgi-bin/webscr" method="post">
                     <input type="hidden" name="edit_selector" data-aid="EDIT_PANEL_EDIT_PAYMENT_ICON" />
@@ -1294,7 +1294,7 @@ class DrawerDetail extends Component {
                   </Button>
                 </Col>
               )}
-              {(userRole === 3 && [EVALUATION, APPOINTMENT, SUBSIDY].includes(event?.type) && event?.flagStatus === ACTIVE && (event?.isPaid || event?.flagInvoice?.data?.[0]?.items?.rate == 0)) && (
+              {(userRole === 3 && [EVALUATION, APPOINTMENT, SUBSIDY].includes(event?.type) && event?.flagStatus === ACTIVE && (event?.sessionInvoice?.isPaid || event?.flagInvoice?.data?.[0]?.items?.rate == 0)) && (
                 <Col span={12}>
                   <Button type='primary' icon={<FaFileContract size={12} />} block onClick={this.onOpenModalCreateNote}>
                     {intl.formatMessage(messages.requestClearance)}
