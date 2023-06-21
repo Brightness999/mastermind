411fb1fefba46a589e984799e613241207582352
wp-dream111 <dmitriymazilin@yahoo.com>
2023-06-20 23:00:45 -0400

    fix join_room socket in MainLayout

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -10,7 +10,6 @@ import { routerLinks } from "routes/constant";
 import { setUser, initializeAuth } from 'src/redux/features/authSlice';
 import { initializeAppointments } from 'src/redux/features/appointmentsSlice';
 import { store } from 'src/redux/store';
-import { socketUrl } from 'utils/api/baseUrl';
 import '../assets/styles/index.less';
 import './styles/main.less';
 
@@ -19,7 +18,6 @@ const { Content, Header } = Layout;
 class MainLayout extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.socket = undefined;
     this.state = {
       route: this.props.routerData.childRoutes
     };
@@ -31,7 +29,6 @@ class MainLayout extends React.PureComponent {
       checkPermission().then(loginData => {
         (loginData?.role > 900 && !window.location.pathname.includes('account/changeprofile') && !window.location.pathname.includes('account/notifications')) && this.props.history.push(routerLinks.Admin);
         store.dispatch(setUser(loginData));
-        this.handleSocketEvents();
       }).catch(err => {
         Cookies.remove('tk');
         this.logout();
@@ -47,29 +44,6 @@ class MainLayout extends React.PureComponent {
   logout = () => {
     store.dispatch(initializeAuth());
     store.dispatch(initializeAppointments());
-  }
-
-  handleSocketEvents = () => {
-    let opts = {
-      query: {
-        token: Cookies.get('tk'),
-      },
-      withCredentials: true,
-      autoConnect: true,
-    };
-    this.socket = io(socketUrl, opts);
-    this.socket.on('connect_error', e => {
-      console.log('connect error ', e);
-    });
-
-    this.socket.on('connect', () => {
-      console.log('socket connect success');
-      this.socket.emit('join_room', this.props.user?._id);
-    });
-
-    this.socket.on('disconnect', e => {
-      console.log('socket disconnect', e);
-    });
   }
 
   render() {
