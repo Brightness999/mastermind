23f92e26bee69279c57101f1eed94d5e02bd28bf
wp-dream111 <dmitriymazilin@yahoo.com>
2023-07-11 08:08:20 -0400

    add role filter in Action Tracker

--- src/routes/Administrator/Statistics/components/index.js
+++ src/routes/Administrator/Statistics/components/index.js
@@ -1,9 +1,9 @@
 import React, { createRef } from 'react';
-import { Button, Divider, Input, Pagination, Space, Table, message, DatePicker } from 'antd';
+import { Button, Divider, Input, Pagination, Space, Table, message, DatePicker, Checkbox } from 'antd';
 import intl from 'react-intl-universal';
 import moment from 'moment';
 import 'moment/locale/en-au';
-import { SearchOutlined } from '@ant-design/icons';
+import { FilterFilled, SearchOutlined } from '@ant-design/icons';
 
 import mgsSidebar from 'components/SideBar/messages';
 import msgMainHeader from 'components/MainHeader/messages';
@@ -29,27 +29,31 @@ export default class extends React.Component {
       actionSearch: '',
       dateRange: undefined,
       loading: false,
+      selectedRoles: [],
     };
     this.searchInput = createRef(null);
   }
 
   componentDidMount() {
-    const { userSearch, actionSearch, pageNumber, pageSize } = this.state;
-    this.getActionList(pageNumber, pageSize, userSearch, actionSearch);
+    this.getActionList();
   }
 
   handleChangePagination = (newPageNumber, newPageSize) => {
-    const { userSearch, actionSearch } = this.state;
-    this.setState({ pageNumber: newPageNumber, pageSize: newPageSize });
-    this.getActionList(newPageNumber, newPageSize, userSearch, actionSearch);
+    this.setState({ pageNumber: newPageNumber, pageSize: newPageSize }, () => {
+      this.getActionList();
+    });
   }
 
-  getActionList(pageNumber = 1, pageSize = 10, userSearch = "", actionSearch = "", dateRange = [0, moment().valueOf()]) {
-    if (!dateRange?.length) {
-      dateRange = [0, moment().valueOf()];
+  getActionList() {
+    const { actionSearch, dateRange, pageNumber, pageSize, selectedRoles, userSearch } = this.state;
+    const postData = {
+      pageNumber, pageSize, userSearch, actionSearch,
+      dateRange: dateRange?.length ? dateRange : [0, moment().valueOf()],
+      roles: selectedRoles,
     }
+
     this.setState({ loading: true });
-    request.post(getTrackedActions, { userSearch, actionSearch, pageNumber, pageSize, dateRange }).then(result => {
+    request.post(getTrackedActions, postData).then(result => {
       this.setState({ loading: false });
       const { success, data } = result;
       if (success) {
@@ -67,13 +71,13 @@ export default class extends React.Component {
   }
 
   render() {
-    const { loading, pageNumber, pageSize, totalSize, trackedLogs, userSearch, actionSearch, dateRange } = this.state;
+    const { loading, pageNumber, pageSize, selectedRoles, totalSize, trackedLogs, userSearch, actionSearch, dateRange } = this.state;
     const roleFilterOptions = [
-      { text: 'Admin', value: 999 },
-      { text: 'Consultant', value: 100 },
-      { text: 'School', value: 60 },
-      { text: 'Provider', value: 30 },
-      { text: 'Parent', value: 3 },
+      { label: 'Admin', value: 999 },
+      { label: 'Consultant', value: 100 },
+      { label: 'School', value: 60 },
+      { label: 'Provider', value: 30 },
+      { label: 'Parent', value: 3 },
     ];
 
     if (store.getState().auth.user?.role === 999) {
@@ -83,20 +87,20 @@ export default class extends React.Component {
     const columns = [
       {
         title: 'User', dataIndex: 'user', key: 'user',
-        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+        filterDropdown: ({ setSelectedKeys, confirm, clearFilters }) => (
           <div style={{ padding: 8 }}>
             <Input
               name='SearchName'
               ref={this.searchInput}
               placeholder={`${intl.formatMessage(msgMainHeader.search)} ${intl.formatMessage(messages.studentName)}`}
-              value={selectedKeys[0]}
+              value={userSearch}
               onChange={e => {
                 setSelectedKeys(e.target.value ? [e.target.value] : []);
                 this.setState({ userSearch: e.target.value });
               }}
               onPressEnter={() => {
+                this.getActionList();
                 confirm();
-                this.getActionList(pageNumber, pageSize, selectedKeys[0], actionSearch, dateRange);
               }}
               style={{ marginBottom: 8, display: 'block' }}
             />
@@ -104,8 +108,8 @@ export default class extends React.Component {
               <Button
                 type="primary"
                 onClick={() => {
+                  this.getActionList();
                   confirm();
-                  this.getActionList(pageNumber, pageSize, selectedKeys[0], actionSearch, dateRange);
                 }}
                 icon={<SearchOutlined />}
                 size="small"
@@ -115,10 +119,11 @@ export default class extends React.Component {
               </Button>
               <Button
                 onClick={() => {
+                  this.setState({ userSearch: '' }, () => {
+                    this.getActionList();
+                  });
                   clearFilters();
                   confirm();
-                  this.getActionList(pageNumber, pageSize, '', actionSearch, dateRange);
-                  this.setState({ userSearch: '' });
                 }}
                 size="small"
                 style={{ width: 90 }}
@@ -150,8 +155,38 @@ export default class extends React.Component {
       },
       {
         title: 'Role', dataIndex: 'user', key: 'role',
-        filters: roleFilterOptions,
-        onFilter: (value, record) => record.user?.role === value,
+        filterDropdown: ({ setSelectedKeys, confirm, clearFilters }) => (
+          <div className='service-dropdown'>
+            <Checkbox.Group
+              options={roleFilterOptions}
+              value={selectedRoles}
+              onChange={(values) => {
+                this.setState({ selectedRoles: values });
+                setSelectedKeys(values);
+              }}
+            />
+            <div className='service-dropdown-footer'>
+              <Button type="primary" size="small" onClick={() => {
+                confirm();
+                this.getActionList();
+              }}>
+                Filter
+              </Button>
+              <Button size="small" onClick={() => {
+                this.setState({ selectedRoles: [] }, () => {
+                  this.getActionList();
+                });
+                clearFilters();
+                confirm();
+              }}>
+                Reset
+              </Button>
+            </div>
+          </div>
+        ),
+        filterIcon: filtered => (
+          <FilterFilled style={{ color: filtered ? '#3E92CF' : undefined }} />
+        ),
         render: (user) => {
           switch (user?.role) {
             case 999: return 'Admin';
@@ -165,20 +200,20 @@ export default class extends React.Component {
       },
       {
         title: 'Action', dataIndex: 'action', key: 'action',
-        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+        filterDropdown: ({ setSelectedKeys, confirm, clearFilters }) => (
           <div style={{ padding: 8 }}>
             <Input
               name='SearchName'
               ref={this.searchInput}
               placeholder={`${intl.formatMessage(msgMainHeader.search)} ${intl.formatMessage(messages.studentName)}`}
-              value={selectedKeys[0]}
+              value={actionSearch}
               onChange={e => {
                 setSelectedKeys(e.target.value ? [e.target.value] : []);
                 this.setState({ actionSearch: e.target.value });
               }}
               onPressEnter={() => {
+                this.getActionList();
                 confirm();
-                this.getActionList(pageNumber, pageSize, userSearch, selectedKeys[0], dateRange);
               }}
               style={{ marginBottom: 8, display: 'block' }}
             />
@@ -186,8 +221,8 @@ export default class extends React.Component {
               <Button
                 type="primary"
                 onClick={() => {
+                  this.getActionList();
                   confirm();
-                  this.getActionList(pageNumber, pageSize, userSearch, selectedKeys[0], dateRange);
                 }}
                 icon={<SearchOutlined />}
                 size="small"
@@ -197,10 +232,11 @@ export default class extends React.Component {
               </Button>
               <Button
                 onClick={() => {
+                  this.setState({ actionSearch: '' }, () => {
+                    this.getActionList();
+                  });
                   clearFilters();
                   confirm();
-                  this.getActionList(pageNumber, pageSize, userSearch, '', dateRange);
-                  this.setState({ actionSearch: '' });
                 }}
                 size="small"
                 style={{ width: 90 }}
@@ -225,7 +261,7 @@ export default class extends React.Component {
       },
       {
         title: 'Date', dataIndex: 'createdAt', key: 'createdAt', type: 'datetime',
-        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+        filterDropdown: ({ setSelectedKeys, confirm, clearFilters }) => (
           <div className='p-3'>
             <div className='mb-5'>
               <RangePicker showTime size='small' value={dateRange} onChange={(range) => {
@@ -237,8 +273,8 @@ export default class extends React.Component {
               <Button
                 type="primary"
                 onClick={() => {
+                  this.getActionList();
                   confirm();
-                  this.getActionList(pageNumber, pageSize, userSearch, actionSearch, selectedKeys);
                 }}
                 icon={<SearchOutlined />}
                 size="small"
@@ -248,10 +284,11 @@ export default class extends React.Component {
               </Button>
               <Button
                 onClick={() => {
+                  this.setState({ dateRange: [] }, () => {
+                    this.getActionList();
+                  });
                   clearFilters();
                   confirm();
-                  this.getActionList(pageNumber, pageSize, userSearch, actionSearch, [0, moment().valueOf()]);
-                  this.setState({ dateRange: [] });
                 }}
                 size="small"
                 style={{ width: 90 }}
