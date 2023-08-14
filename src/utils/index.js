af4abf213c8e53061e01d3af8e4e0777b83d0faa
wp-dream111 <dmitriymazilin@yahoo.com>
2023-08-14 16:06:28 -0400

    change paypal account

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
 export const PAYPAL_INFO = {
-  // clientId: 'AUrXf0bQPW3v2qSaLxeI1-ybg55jVGf4GVzR1lSEZrZtBT1zlO_7jMI5GGrj2zm6UaaSOyVBPyOqp2aQ'
-  clientId: 'AQfOUMQxT7AWSrhF1o2Q2WDJ-Fv-DKylk5ORlyJjuwPR9EDKDlpU79k2WSRtiiUxAFcP5JJpHYoLTzk-',
+  clientId: 'AUrXf0bQPW3v2qSaLxeI1-ybg55jVGf4GVzR1lSEZrZtBT1zlO_7jMI5GGrj2zm6UaaSOyVBPyOqp2aQ'
+  // clientId: 'AQfOUMQxT7AWSrhF1o2Q2WDJ-Fv-DKylk5ORlyJjuwPR9EDKDlpU79k2WSRtiiUxAFcP5JJpHYoLTzk-',
 }
